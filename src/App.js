3a09936e3b5495df43a1096f711e2d3eed8e2d16
raghubir kujur <raghubir@raghav-mac-book.local>
2024-02-02 14:09:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className='flex text-center gap-3'>
+    <navbar className='flex gap-3'>
+      <a>Home</a>
+      <a>About Us</a>
+      <a>Services</a>
+      <a>Contact Us</a>
+    </navbar>
+    </div>
+    </>
+  );
+}
+
+export default App;
